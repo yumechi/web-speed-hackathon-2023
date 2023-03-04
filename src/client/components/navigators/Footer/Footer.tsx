622312ba3d7f5027ee43a306da3850917e9e18b8
yumechi <yumechi0525@gmail.com>
2023-03-05 08:36:00 +0900

    :recycle: ロゴファイルを小さくする

--- src/client/components/navigators/Footer/Footer.tsx
+++ src/client/components/navigators/Footer/Footer.tsx
@@ -28,7 +28,7 @@ export const Footer: FC = () => {
               ))}
             </ul>
             <NavLink to="/">
-              <Image src="/icons/logo.svg" />
+              <Image src="/icons/logo.jpg" />
             </NavLink>
           </footer>
         );
